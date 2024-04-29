cdcdfe73fb1729ae1dbf6c69a97dbc878b066fe5
FUZULU20 <guliyeffizuli20@gmail.com>
2024-04-29 22:57:38 +0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static task1.Program
+
+public class Class1
+{
+	public Class1()
+	{
+
+        string max = massiv[0];
+        for (int i = 1; i < length; i++)
+        {
+            if (string.Compare(massiv[i], max) > 0)
+            {
+                max = massiv[i];
+            }
+        }
+
+        Console.WriteLine("En büyük değer: " + max);
+
+
+
+        string min = massiv[0];
+        for (int i = 1; i < length; i++)
+        {
+            if (string.Compare(massiv[i], min) < 0)
+            {
+                min = massiv[i];
+            }
+        }
+
+        Console.WriteLine("En küçük değer: " + min);
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int length = 1;
+        bool okeylength = false;
+
+        while (!okeylength)
+        {
+            Console.WriteLine("Massivin uzunluğunu girin:");
+            string lengthEntry = Console.ReadLine();
+
+            if (int.TryParse(lengthEntry, out length))
+            {
+                okeylength = true;
+            }
+            else
+            {
+                Console.WriteLine("Reqem tipli dəyər daxil edin");
+            }
+        }
+
+        string[] massiv = new string[length];
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++)
+        {
+            massiv[i] = random.Next(1000).ToString();
+            Console.WriteLine(massiv[i]);
+        }
+        
+    }
+}
